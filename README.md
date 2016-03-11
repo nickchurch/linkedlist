4564f42b68cf21d0774ce448169edd3220d7fc5d
nickchurch <nickchurch10@gmail.com>
2016-03-11 12:33:13 -0800

    Added v1 of the sql schema Added our v1 API documentation

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # linkedlist
 A social shopping list
 
-Requirements:
+Server code requirements:
 	Python v2.7.10
 	Flask v0.10.1
 	SQLAlchemy v1.0.12
---
+++ server/schema.sql
@@ -0,0 +1,38 @@
+PRAGMA foreign_keys = ON;
+
+drop table if exists user;
+create table user (
+  id integer primary key autoincrement,
+  name text not null,
+  email text not null unique,
+  auth_token text not null
+);
+
+drop table if exists list;
+create table list (
+  id integer primary key autoincrement,
+  owner_id integer references user(id) not null on delete cascade,
+  name text not null
+);
+
+drop table if exists list_item;
+create table list_item (
+  id integer primary key autoincrement,
+  list_id integer references list(id) not null on delete cascade,
+  value text not null,
+  checked boolean
+);
+
+drop table if exists list_member;
+create table list_member (
+  id integer primary key autoincrement,
+  list_id integer references list(id) not null on delete cascade,
+  user_id integer references user(id) not null on delete cascade
+);
+
+drop table if exists list_item_relation;
+create table list_item_relation (
+  id integer primary key autoincrement,
+  list_id integer references list(id) not null on delete cascade,
+  item_id integer references list_item(id) not null on delete cascade
+);
