29da88d200fb84ac8377c2fe3c295d4ac3eacb9f
Daniel Farley <dfarley@ucsc.edu>
2016-03-12 19:38:41 -0800

    Merge branch 'master' of https://github.com/nickchurch/linkedlist
    # Conflicts:
    #	androidApp/app/src/main/java/a342com/linkedlist/MainActivity.java
    
    Finished MainActivity and started ListActivity.  Layout needs "pull to refresh" and several functions are incomplete.  Additionally, we can't make the JSON response structure without knowing the exact response format, waiting on Nick for that.

--- androidApp/app/src/main/java/a342com/linkedlist/ListActivity.java
+++ androidApp/app/src/main/java/a342com/linkedlist/ListActivity.java
@@ -1,13 +1,100 @@
 package a342com.linkedlist;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.content.Intent;
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.List;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.GsonConverterFactory;
+import retrofit2.Retrofit;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+import android.location.Location;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
 
 public class ListActivity extends AppCompatActivity {
+
+    public static String LOG_TAG = "My log tag";
+    public static final String MY_PREFS_NAME = "MyPrefsFile";
+
+    public static String nickname = "";
+    public static String email = "";
+    public static String auth_token = "";
+    public static String password = "";
+
+    public ArrayList<RoomElement> roomList;
+    public MyAdapter aa;
+
+    private class RoomElement {
+        public String list_id;
+        public String list_name;
+
+        RoomElement(String _id, String _name) {
+            list_id = _id;
+            list_name = _name;
+        }
+    }
+
+    private class MyAdapter extends ArrayAdapter<RoomElement> {
+        int resource;
+        Context context;
+
+        public MyAdapter (Context _context, int _resource, List<RoomElement> _rooms) {
+            super(_context, _resource, _rooms);
+            resource = _resource;
+            context = _context;
+            this.context = _context;
+        }
+
+        @Override
+        public View getView (int position, View convertView, ViewGroup parent) {
+            LinearLayout newView;
+            RoomElement w = getItem(position);
+
+            if (convertView == null) {
+                newView = new LinearLayout(getContext());
+                String inflater = Context.LAYOUT_INFLATER_SERVICE;
+                LayoutInflater vi = (LayoutInflater) getContext().getSystemService(inflater);
+                vi.inflate(resource, newView, true);
+            } else {
+                newView = (LinearLayout) convertView;
+            }
+
+            TextView txt_room_name = (TextView) newView.findViewById(R.id.room_item_name);
+            txt_room_name.setText(w.list_name);
+
+            return newView;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +111,116 @@ public class ListActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        SharedPreferences prefs = getSharedPreferences(MY_PREFS_NAME, 0);
+        nickname = prefs.getString("username", "");
+        email = prefs.getString("email", "");
+        auth_token = prefs.getString("auth_token", "");
+        password = prefs.getString("password", "");
+
+        ((CollapsingToolbarLayout)findViewById(R.id.toolbar_layout)).setTitle(nickname + "'s Lists");
+
+        refreshList();
+    }
+
+    public void refreshList(View v) {refreshList();}
+    public void refreshList() {
+        aa.clear();
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient httpClient = new OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(getString(R.string.http_base_url))
+                .addConverterFactory(GsonConverterFactory.create())    //parse Gson string
+                .client(httpClient)    //add logging
+                .build();
+        ListsService service = retrofit.create(ListsService.class);
+        Call<ListsResponse> queryLists = service.getLists(auth_token);
+
+        queryLists.enqueue(new Callback<ListsResponse>() {
+            @Override
+            public void onResponse(Response<ListsResponse> response) {
+                if (response.body() != null && response.body().result.equals("ok")) {
+                    List<Room> roomsResponse = response.body().resultList;
+                    while (!roomsResponse.isEmpty()) {
+                        Room elem = roomsResponse.remove(roomsList.size()-1);
+                        //TODO
+                        RoomElement le = new RoomElement(
+                                elem.list_id,
+                                elem.list_name
+                        );
+                        roomList.add(le);
+                    }
+                    aa.notifyDataSetChanged();
+                } else {
+                    //TODO: error
+                }
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                Toast.makeText(
+                        getApplicationContext(),
+                        "Could not connect to server!\n\n"
+                                + "ListActivity().ListsResponse.onFailure():\n"
+                                + t.toString(),
+                        Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
+    }
+
+
+    public void createList(View v) {createList();}
+    public void createList() {
+        //TODO
+    }
+
+    public void removeList(View v) {removeList();}
+    public void removeList() {
+        //TODO
+    }
+
+    public void gotoList(View v) {
+        //TODO
+    }
+
+    public interface ListsService {
+        @GET("lists")
+        Call<ListsResponse> getLists(@Query("session_api_key") String auth_token);
+
+
+        @GET("list/removeuser")
+        Call<ListsResponse> leaveList(@Query("session_api_key") String auth_token,
+                                      @Query("list_id") String list_id,
+                                      @Query("user_id") String user_id);
+
+
+        @GET("") //TODO: not implemented
+        Call<LoginResponse> createList(@Query("email_address") String email,
+                                       @Query("password") String password,
+                                       @Query("password_conf") String password_conf,
+                                       @Query("username") String nickname);
+    }
+
+}
+
+class ListsResponse {
+    @SerializedName("result")
+    @Expose
+    public String result;
+    @SerializedName("linkedlists")
+    @Expose
+    public List<Room> resultList = new ArrayList<Room>();
+
+
 }
--- androidApp/app/src/main/java/a342com/linkedlist/MainActivity.java
+++ androidApp/app/src/main/java/a342com/linkedlist/MainActivity.java
@@ -10,37 +10,65 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.content.Intent;
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.List;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.GsonConverterFactory;
+import retrofit2.Retrofit;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+import android.location.Location;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 
 public class MainActivity extends AppCompatActivity {
 
     public static String LOG_TAG = "My log tag";
     public static final String MY_PREFS_NAME = "MyPrefsFile";
 
-    public static String username = "";
+    public static String nickname = "";
     public static String email = "";
     public static String auth_token = "";
+    public static String password = "";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //test
+
         SharedPreferences prefs = getSharedPreferences(MY_PREFS_NAME, 0);
-        username = prefs.getString("username", "None");
-        email = prefs.getString("email", "None");
-        auth_token = prefs.getString("auth_token", "None");
+        nickname = prefs.getString("username", "");
+        email = prefs.getString("email", "");
+        auth_token = prefs.getString("auth_token", "");
+        password = prefs.getString("password", "");
     }
 
     @Override
     public void onResume(){
-        Log.i(LOG_TAG, "Inside resume of main activity");
-
-        if(auth_token.equals("None")) {
-        } else {
-        }
-
         super.onResume();
+        Log.i(LOG_TAG, "Checking for previous login:" + auth_token);
+        if(!auth_token.equals("")) {
+            goto_ListActivity();
+        }
     }
 
     @Override
@@ -49,32 +77,186 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences.Editor editor = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
         editor.putString("auth_token", auth_token);
         editor.putString("email", email);
-        editor.putString("username", username);
+        editor.putString("username", nickname);
+        editor.putString("password", password);
         editor.commit();
 
         super.onPause();
     }
 
-    public void linkedList(View v){
-        Context context = getApplicationContext();
-        CharSequence text = "";
-        int duration = Toast.LENGTH_SHORT;
+    public void login_login(View v) {login_login();}
+    public void login_login(){
 
-        Bundle id = new Bundle();
-        id.putString("auth_token", auth_token);
+        email = ((EditText) findViewById(R.id.login_edt_email)).getText().toString();
+        password = ((EditText) findViewById(R.id.login_edt_password)).getText().toString();
 
-        Intent intent = new Intent(this, ListActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
+        if ((email.length() == 0) || (password.length() == 0)) {
+            Toast.makeText(
+                    getApplicationContext(),
+                    "Please fill out all fields",
+                    Toast.LENGTH_LONG)
+                    .show();
+        } else {
+            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+            OkHttpClient httpClient = new OkHttpClient.Builder()
+                    .addInterceptor(logging)
+                    .build();
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(getString(R.string.http_base_url))
+                    .addConverterFactory(GsonConverterFactory.create())    //parse Gson string
+                    .client(httpClient)    //add logging
+                    .build();
+            LoginService login_service = retrofit.create(LoginService.class);
+            Call<LoginResponse> queryLogin = login_service.LoginResponse(email, password);
 
-        int len = editText.getText().length();
+            queryLogin.enqueue(new Callback<LoginResponse>() {
+                @Override
+                public void onResponse(Response<LoginResponse> response) {
+                    if (response.body() != null && response.body().result.equals("ok")) {
+                        if (response.body().authTok.equals("None")) {
+                            Toast.makeText(
+                                    getApplicationContext(),
+                                    "Invalid email address or password\nPlease try again.",
+                                    Toast.LENGTH_LONG)
+                                    .show();
+                            ((EditText) findViewById(R.id.login_edt_password)).setText("");
+                        } else {
+                            auth_token = response.body().authTok;
+                            goto_ListActivity();
+                        }
+                    } else {
+                        Toast.makeText(
+                                getApplicationContext(),
+                                "Bad server response!\n\n"
+                                        + "MainActivity().LoginResponse.onFailure():\n"
+                                        + response.raw().code(),
+                                Toast.LENGTH_LONG)
+                                .show();
+                    }
+                }
 
-        if(len == 0){
-            text = "Please Fill Out Each Field!";
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
-        }
-        else{
-            startActivity(intent);//pass the cuisine to the search activity for searching
+                @Override
+                public void onFailure(Throwable t) {
+                    Toast.makeText(
+                            getApplicationContext(),
+                            "Could not connect to server!\n\n"
+                                    + "MainActivity().LoginResponse.onFailure():\n"
+                                    + t.toString(),
+                            Toast.LENGTH_LONG)
+                            .show();
+                }
+            });
+
+
+            //if auth_token == none
+            //toast "invalid login"
+            //else
+            //store email, password, auth_token
+
         }
     }
+
+    public void login_create(View v) {
+        findViewById(R.id.layout_login).setVisibility(View.GONE);
+        findViewById(R.id.layout_create).setVisibility(View.VISIBLE);
+    }
+
+    public void create_create(View v) {
+        email = ((EditText) findViewById(R.id.create_edt_email)).getText().toString();
+        password = ((EditText) findViewById(R.id.create_edt_password)).getText().toString();
+        nickname = ((EditText) findViewById(R.id.create_edt_nickname)).getText().toString();
+
+        if ((email.length() == 0) || (password.length() == 0) || (nickname.length() == 0)) {
+            Toast.makeText(
+                    getApplicationContext(),
+                    "Please fill out all fields",
+                    Toast.LENGTH_LONG)
+                    .show();
+        } else {
+            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+            OkHttpClient httpClient = new OkHttpClient.Builder()
+                    .addInterceptor(logging)
+                    .build();
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(getString(R.string.http_base_url))
+                    .addConverterFactory(GsonConverterFactory.create())    //parse Gson string
+                    .client(httpClient)    //add logging
+                    .build();
+            LoginService login_service = retrofit.create(LoginService.class);
+            Call<LoginResponse> queryLogin = login_service.CreateResponse(email, password, password, nickname);
+
+            queryLogin.enqueue(new Callback<LoginResponse>() {
+                @Override
+                public void onResponse(Response<LoginResponse> response) {
+                    if (response.body() != null && response.body().result.equals("ok")) {
+                        if (response.body().authTok.equals("None")) {
+                            Toast.makeText(
+                                    getApplicationContext(),
+                                    "Invalid email address or password\nPlease try again.",
+                                    Toast.LENGTH_LONG)
+                                    .show();
+                            ((EditText) findViewById(R.id.login_edt_password)).setText("");
+                        } else {
+                            auth_token = response.body().authTok;
+                            goto_ListActivity();
+                        }
+                    } else {
+                        Toast.makeText(
+                                getApplicationContext(),
+                                "Bad server response!\n\n"
+                                        + "MainActivity().LoginResponse.onFailure():\n"
+                                        + response.raw().code(),
+                                Toast.LENGTH_LONG)
+                                .show();
+                    }
+                }
+
+                @Override
+                public void onFailure(Throwable t) {
+                    Toast.makeText(
+                            getApplicationContext(),
+                            "Could not connect to server!\n\n"
+                                    + "MainActivity().LoginResponse.onFailure():\n"
+                                    + t.toString(),
+                            Toast.LENGTH_LONG)
+                            .show();
+                }
+            });
+
+        }
+    }
+
+    public void goto_ListActivity() {
+        SharedPreferences.Editor editor = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
+        editor.putString("auth_token", auth_token);
+        editor.putString("email", email);
+        editor.putString("username", nickname);
+        editor.putString("password", password);
+        editor.commit();
+
+        Intent intent = new Intent(this, ListActivity.class);
+        startActivity(intent);
+    }
+
+    public interface LoginService {
+        @GET("user/login")
+        Call<LoginResponse> LoginResponse(@Query("email") String email,
+                                           @Query("password") String password);
+        @GET("user/createaccount")
+        Call<LoginResponse> CreateResponse(@Query("email_address") String email,
+                                          @Query("password") String password,
+                                          @Query("password_conf") String password_conf,
+                                          @Query("username") String nickname);
+    }
+}
+
+class LoginResponse {
+    @SerializedName("result")
+    @Expose
+    public String result;
+    @SerializedName("session_api_key")
+    @Expose
+    public String authTok;
 }
